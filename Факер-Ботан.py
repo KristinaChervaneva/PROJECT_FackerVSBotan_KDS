c624b5f12c1b5113f16f99fecf554e2e492b4327
DenisDedIvanov <113638611+DenisDedIvanov@users.noreply.github.com>
2022-12-18 18:09:48 +0300

    стрельба и столкновение

--- Факер-Ботан.py
+++ Факер-Ботан.py
@@ -22,6 +22,13 @@ class Man:
         self.rect = pygame.Rect(px, py, TILE, TILE) #координаты, ширина и высота
         self.direct = direct
         self.moveSpeed = 2
+        self.hp = 5
+
+        #параметры пули, учитивыющие урон, скорость и задержку
+        self.shotTimer = 0
+        self.shotDelay = 60
+        self.bulletSpeed = 5
+        self.bulletDamage = 1
 
         #кнопки управления
         self.keyLEFT = keyList[0]
@@ -44,12 +51,55 @@ class Man:
             self.rect.y += self.moveSpeed
             self.direct = 2
 
+        #механика стрельбы
+        if keys[self.keySHOT] and self.shotTimer == 0:
+            dx = DIRECTS[self.direct][0] * self.bulletSpeed
+            dy = DIRECTS[self.direct][1] * self.bulletSpeed
+            Bullet(self, self.rect.centerx, self.rect.centery, dx, dy, self.bulletDamage)
+            self.shotTimer = self.shotDelay
+
+        if self.shotTimer > 0: self.shotTimer -= 1
+
     def draw(self): #рисовка
         pygame.draw.rect(window, self.color, self.rect)
 
         x = self.rect.centerx + DIRECTS[self.direct][0] * 30
         y = self.rect.centery + DIRECTS[self.direct][1] * 30
         pygame.draw.line(window, 'white', self.rect.center, (x, y), 4)
+
+    #метод насения урона
+    def damage(self, value):
+        self.hp -= value
+        if self.hp <= 0:#смерть
+            objects.remove(self)
+            print(self.color, 'dead')
+
+class Bullet:#класс пули
+    def __init__(self, parent, px, py, dx, dy, damage):#parent- от кого идет пуля, р_х, р_у-координаты начала пули, d_x,d_y- куда летит пуля, damage- урон
+        bullets.append(self)
+        self.parent = parent
+        self.px, self.py = px, py
+        self.dx, self.dy = dx, dy
+        self.damage = damage
+
+    def update(self):#изменение параметров
+        self.px += self.dx
+        self.py += self.dy
+
+        #механизм удаления пули, если она вылетела за пределы игрового поля и при попадании в объект
+        if self.px < 0 or self.px > WIDTH or self.py < 0 or self.py > HEIGHT:
+            bullets.remove(self)
+        else:
+            for obj in objects:
+                if obj != self.parent and obj.rect.collidepoint(self.px, self.py):
+                    obj.damage(self.damage)
+                    bullets.remove(self)
+                    break
+
+    def draw(self): #параметры пули
+        pygame.draw.circle(window, 'yellow', (self.px, self.py), 3)
+
+bullets = []
 
 
 objects = [] #тут храним все объекты, которые используем в игре
@@ -63,11 +113,14 @@ while play:
             play = False
 
     keys = pygame.key.get_pressed()
+    for bullet in bullets: bullet.update()
 
     for obj in objects: obj.update()
 
     window.fill('black') #закрашиваем фон в черный
     for obj in objects: obj.draw()
+
+    for bullet in bullets: bullet.draw()
 
     pygame.display.update()
     clock.tick(FPS)
