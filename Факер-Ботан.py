37ce6879d88942b1d32df252078dd8c4a6552e1c
KristinaChervaneva <chervaneva.kristina@gmail.com>
2022-12-18 19:58:03 +0300

    разделила людей на факеров и ботанов

--- Факер-Ботан.py
+++ Факер-Ботан.py
@@ -32,7 +32,73 @@ class UI:
                 rect = text.get_rect(center = (5 + i * 70 + 32, 5 + 11))
                 window.blit(text, rect)
                 i += 1
-class Man: #класс человека
+class Facker: #класс факеров
+    def __init__(self, color, px, py, direct, keyList): #px, py - позиция, direct - направление, куда смотрит человек, keyList - список кнопок
+        objects.append(self) #добавляем ссылку на класс Man в список объектов
+        self.type = 'man'
+
+        self.color = color
+        self.rect = pygame.Rect(px, py, TILE, TILE) #координаты, ширина и высота
+        self.direct = direct
+        self.moveSpeed = 2
+        self.hp = 5
+
+        #параметры пули, учитивыющие урон, скорость и задержку
+        self.shotTimer = 0
+        self.shotDelay = 60
+        self.bulletSpeed = 5
+        self.bulletDamage = 1
+
+        #кнопки управления
+        self.keyLEFT = keyList[0]
+        self.keyRIGHT = keyList[1]
+        self.keyUP = keyList[2]
+        self.keyDOWN = keyList[3]
+        self.keySHOT = keyList[4]
+
+    def update(self): #обновление статусов
+        oldX, oldY = self.rect.topleft #сохраняем старую позицию
+        if keys[self.keyLEFT]: #движение влево
+            self.rect.x -= self.moveSpeed
+            self.direct = 3
+        elif keys[self.keyRIGHT]: #движение вправо
+            self.rect.x += self.moveSpeed
+            self.direct = 1
+        elif keys[self.keyUP]: #движение вверх
+            self.rect.y -= self.moveSpeed
+            self.direct = 0
+        elif keys[self.keyDOWN]: #движение вниз
+            self.rect.y += self.moveSpeed
+            self.direct = 2
+
+        for obj in objects: #столкновение человека с блоком
+            if obj != self and self.rect.colliderect(obj.rect):
+                self.rect.topleft = oldX, oldY
+
+        #механика стрельбы
+        if keys[self.keySHOT] and self.shotTimer == 0:
+            dx = DIRECTS[self.direct][0] * self.bulletSpeed
+            dy = DIRECTS[self.direct][1] * self.bulletSpeed
+            Bullet(self, self.rect.centerx, self.rect.centery, dx, dy, self.bulletDamage)
+            self.shotTimer = self.shotDelay
+
+        if self.shotTimer > 0: self.shotTimer -= 1
+
+    def draw(self): #рисовка
+        pygame.draw.rect(window, self.color, self.rect)
+
+        x = self.rect.centerx + DIRECTS[self.direct][0] * 30
+        y = self.rect.centery + DIRECTS[self.direct][1] * 30
+        pygame.draw.line(window, 'white', self.rect.center, (x, y), 4)
+
+    #метод насения урона
+    def damage(self, value):
+        self.hp -= value
+        if self.hp <= 0:#смерть
+            objects.remove(self)
+            print(self.color, 'dead')
+
+class Botan: #класс ботанов
     def __init__(self, color, px, py, direct, keyList): #px, py - позиция, direct - направление, куда смотрит человек, keyList - список кнопок
         objects.append(self) #добавляем ссылку на класс Man в список объектов
         self.type = 'man'
@@ -146,8 +212,8 @@ bullets = []
 
 
 objects = [] #тут храним все объекты, которые используем в игре
-Man('blue', 100, 275, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE))
-Man('red', 650, 275, 0, (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_RSHIFT))
+Facker('blue', 100, 275, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE))
+Botan('red', 650, 275, 0, (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_RSHIFT))
 ui = UI()
 
 for _ in range(50): #расставляем блоки на поле рандомным образом
